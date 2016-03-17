fa918e6e5dc12a2d9e108f68419d84175fe1cfbe
David Alexander <dalexander@pacificbiosciences.com>
2016-03-17 13:48:27 -0700

    Fix gridSMP

--- bauhaus/pflow/pflow.py
+++ bauhaus/pflow/pflow.py
@@ -92,7 +92,7 @@ class PFlow(ContextTracker):
             w.newline()
             w.variable("ncpus", "8")
             w.variable("grid", "qsub -sync y -cwd -V -b y")
-            w.variable("gridSMP", "$grid -pe smp $ncpus")
+            w.variable("gridSMP", "$grid -pe smp")
             w.newline()
             w.comment("Rules")
             w.newline()
