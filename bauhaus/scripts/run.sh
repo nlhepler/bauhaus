778071ced65aaa651b80a1c1467e11058c9fc7ee
David Alexander <dalexander@pacificbiosciences.com>
2016-04-07 14:02:07 -0700

    Use the R/3.2.3-bauhaus module, which has extra libs we need

--- bauhaus/scripts/run.sh
+++ bauhaus/scripts/run.sh
@@ -5,7 +5,7 @@ MODULEPATH=/pbi/dept/primary/modulefiles:$MODULEPATH
 module purge
 module load smrtanalysis/mainline
 module load gfftools/dalexander
-module load R/3.2.3-experimental
+module load R/3.2.3-bauhaus
 
 THISDIR=$(cd "$(dirname "$0")" && pwd)
 cd $THISDIR
