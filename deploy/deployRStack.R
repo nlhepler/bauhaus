425b457d2691a6bbd4c9039556a019961c2f475b
David Alexander <dalexander@pacificbiosciences.com>
2016-05-12 13:25:06 -0700

    Fix feather dep

--- deploy/deployRStack.R
+++ deploy/deployRStack.R
@@ -1,3 +1,3 @@
 library(devtools)
 install_github("PacificBiosciences/pbbamr", build_vignettes = FALSE)
-install_github("wesm/feather/R",  build_vignettes = FALSE, ref="0.2")
+install_github("wesm/feather/R",  build_vignettes = FALSE, ref="v0.2.0")
